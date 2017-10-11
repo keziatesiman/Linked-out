fbee0c2a34ccd5d7264fe646be30c7545fd5d46d
Kezia Irene <irenekezia98@gmail.com>
2017-10-11 21:09:17 +0700

    revisi pembuatan objek

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,24 +2,23 @@ from django.shortcuts import render
 from .models import Person, Expertise, PhotoURL
 
 # Create your views here.
-#name = 'Hepzibah Smith' 
-#gender = 'Female'
-#expertise = [" Play piano ", " Play violin ", " Play flute ", " Music arragement "," Music composition ", " Film Scoring "]
-#description = 'Antique expert. Experience as marketer for 10 years'
-#email = 'hello@smith.com'
+
+person = Person.objects.create(name="Johann Sebastian Bach", birthdate="1685-03-31", gender="Male", description="Master of contrapuntal composition and professional harpsichord player", email="jsbach@gmail.com")
+expertise = Expertise.objects.create(expertise ="Contrapuntal composition")
+expertise = Expertise.objects.create(expertise ="Church leading musician")
+expertise = Expertise.objects.create(expertise ="Glorifying God")
+model_pic = expertise = PhotoURL.objects.create(model_pic ="http://www.bachsociety.org/wp-content/uploads/2017/01/Johann-sunglasses.jpg")
 
 def index(request):
     html = 'user_profile.html'
     person = Person.objects.all()
 
-    print("-------------------------------")
-    print(Person.objects.all().count())
-
     response = {} #TODO Implement yourname
+    response['author']= "Kezia Irene"
     response['name'] = Person.objects.all()[0].name
     response['birthdate'] = Person.objects.all()[0].birthdate
     response['gender'] = Person.objects.all()[0].gender
-    response['expertise'] = Expertise.objects.all()
+    response['expertise'] = Expertise.objects.all()[0:3]
     response['description'] = Person.objects.all()[0].description
     response['email'] = Person.objects.all()[0].email
     response['model_pic'] = PhotoURL.objects.all()[0].model_pic
